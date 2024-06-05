b8b19265084a8009d8d019039e2920cf4ce4c728
narain <narainp@outlook.com>
2024-06-05 16:08:48 -0700

    updated testing

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 import ctypes
 import numpy as np
+import time
 
 # Load the shared library
 lib = ctypes.CDLL('./lib.so')
@@ -11,6 +12,18 @@ lib.mm_naive.argtypes = [np.ctypeslib.ndpointer(dtype=np.float32, ndim=2, flags=
                          ctypes.c_int]
 lib.mm_naive.restype = None
 
+lib.mm_transpose.argtypes = [np.ctypeslib.ndpointer(dtype=np.float32, ndim=2, flags='C_CONTIGUOUS'),
+                         np.ctypeslib.ndpointer(dtype=np.float32, ndim=2, flags='C_CONTIGUOUS'),
+                         np.ctypeslib.ndpointer(dtype=np.float32, ndim=2, flags='C_CONTIGUOUS'),
+                         ctypes.c_int]
+lib.mm_transpose.restype = None
+
+lib.mm_threads.argtypes = [np.ctypeslib.ndpointer(dtype=np.float32, ndim=2, flags='C_CONTIGUOUS'),
+                         np.ctypeslib.ndpointer(dtype=np.float32, ndim=2, flags='C_CONTIGUOUS'),
+                         np.ctypeslib.ndpointer(dtype=np.float32, ndim=2, flags='C_CONTIGUOUS'),
+                         ctypes.c_int]
+lib.mm_threads.restype = None
+
 # Initialize matrices
 n = 1024
 a = np.random.randn(n, n).astype(np.float32)
@@ -18,7 +31,20 @@ b = np.random.randn(n, n).astype(np.float32)
 c = np.zeros((n, n), dtype=np.float32)
 
 # Call the function
+start = time.perf_counter()
 lib.mm_naive(a, b, c, n)
+end = time.perf_counter()
 
-print("Result of matrix multiplication:")
-print(c)
+print("time taken mm_naive : ", end - start)
+
+start = time.perf_counter()
+lib.mm_transpose(a, b, c, n)
+end = time.perf_counter()
+
+print("time taken mm transpose: ", end - start)
+
+start = time.perf_counter()
+lib.mm_threads(a, b, c, n)
+end = time.perf_counter()
+
+print("time taken mm threads: ", end - start)
