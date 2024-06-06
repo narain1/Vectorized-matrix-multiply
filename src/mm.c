3b3e06e5a00188026cc65030c84f34c670d9beea
narain1 <narainp@outlook.com>
2024-06-06 02:00:53 -0700

    added simd intrinsics

--- src/mm.c
+++ src/mm.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <immintrin.h>
 
 #define TILE_SIZE 8  // Define the size of the tile
+float hsum(__m256 v) 
+{
+  __m256 shuf = _mm256_shuffle_ps(v, v, _MM_SHUFFLE(1, 0, 3, 2));
+  __m256 sums = _mm256_add_ps(v, shuf);
+  shuf = _mm256_movehdup_ps(sums);
+  sums = _mm256_add_ps(sums, shuf);
+  return _mm256_cvtss_f32(sums);
+}
 
 void mm_naive(float *a, float *b, float *c, int n)
 {
@@ -53,4 +62,20 @@ void mm_tiled_omp(float *a, float *b, float *c, int n)
             }
         }
     }
+}
+
+void mm_vector(float *a, float *b, float *c, int n)
+{
+  for (int i=0; i<n; ++i) {
+    #pragma unroll
+    for (int j=0; j<n; ++j) {
+      __m256 sum = _mm256_setzero_ps();
+      for (int k=0; k<n; k+= 8) {
+        __m256 vecA = _mm256_loadu_ps(&a[i * n + k]);
+        __m256 vecB = _mm256_loadu_ps(&b[k * n + j]);
+        sum = _mm256_fmadd_ps(vecA, vecB, sum);
+      }
+      c[i * n + j] = hsum(sum);
+    }
+  }
 }
